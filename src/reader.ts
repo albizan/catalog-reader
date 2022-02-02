8aed2582a166d83debbd278f57fc83ec73ee29ff
Alberto Zanotti <albizan@hotmail.it>
2022-02-02 19:56:57 +0100

    Write on file

--- src/reader.ts
+++ src/reader.ts
@@ -3,11 +3,16 @@ import path from "path";
 import readLine from "readline";
 import { once } from "events";
 import { createReadStream } from "fs";
+const fs = require("fs");
+
+const USD_TO_EUR = 0.89;
+const POUNDS_TO_KG = 0.45;
 
 @Service()
 export class Reader {
   private filePath: string = path.resolve(__dirname, "../files/catalog.csv");
   private counter = 0;
+  private products = "";
 
   async read() {
     console.log("Reading catalog...");
@@ -26,29 +31,32 @@ export class Reader {
         let reversed = chunks[0].split("").reverse().join("").slice(2);
         let values: string[] = reversed.split(" ,", 4);
         values = values.map((s) => s.trim().split("").reverse().join(""));
-        const images: string = chunks[1];
-        const product: string = values[3];
-        const price: number = Number(values[2]);
+        const imageUrl: string = chunks[1].split(",")[0].split("?")[0];
+        const product: string = values[3].substring(0, 120);
+        const price: string = (Number(values[2]) * USD_TO_EUR).toFixed(2);
         const category: string = values[1];
-        const weight: number = Number(values[0]);
-        if (isNaN(price)) {
+        const weight: string = (Number(values[0]) * POUNDS_TO_KG).toFixed(2);
+
+        if (isNaN(Number(price))) {
           //console.log(chunks[0]);
           console.log("Anomaly at line", this.counter);
         }
-        console.log(product);
-
-        /* console.log({
-          product,
-          price,
-          weight,
-          category,
-        }); */
+        // console.log(images);
+        const row = `${product};1;10;${price};${weight};99;${imageUrl}\r\n`;
+        this.products += row;
       }
       this.counter++;
     });
 
     // Stop process till a close event is fired on rl stream emitter
     await once(rl, "close");
+
+    fs.writeFile("./files/products.csv", this.products, (err: any) => {
+      if (err) console.log(err);
+      else {
+        // console.log("File written successfully\n");
+      }
+    });
 
     console.timeEnd("read");
   }
